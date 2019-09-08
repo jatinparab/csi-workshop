5f2e5944cdf3c9b0749d8251ff7f133cfe148f2f
Jatin Parab <sulphurgfx@gmail.com>
2019-09-08 17:49:24 +0530

    code cleanup

--- grabit/forms.py
+++ grabit/forms.py
@@ -6,14 +6,19 @@ from django import forms
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Enter username'})
-        self.fields['password1'].widget = forms.PasswordInput(attrs = {'class': 'form-control', 'placeholder': 'Enter password'})
-        self.fields['password2'].widget = forms.PasswordInput(attrs = {'class': 'form-control', 'placeholder': 'Confirm password'})
-        self.fields['first_name'].widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Enter first name'})
-        self.fields['last_name'].widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Enter last name'})
-        self.fields['email'].widget = forms.EmailInput(attrs = {'class': 'form-control', 'placeholder': 'Enter email'})
-
+        self.fields['username'].widget = forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter username'})
+        self.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter password'})
+        self.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Confirm password'})
+        self.fields['first_name'].widget = forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter first name'})
+        self.fields['last_name'].widget = forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter last name'})
+        self.fields['email'].widget = forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter email'})
 
     class Meta:
         model = User
-        fields = ( 'username', 'first_name', 'last_name', 'email' )
+        fields = ('username', 'first_name', 'last_name', 'email')
--- grabit/views.py
+++ grabit/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response, reverse, redirect, render
+from django.shortcuts import render_to_response, redirect, render
 from django.urls import resolve
 from .decorators import find_worth
 from .services import get_items, grab_item
@@ -13,13 +13,13 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 @login_required
 @find_worth
 def home(request):
     items = get_items()
     response = {}
     response['items'] = items
-    response['name'] = reverse('home')
     return render_to_response('home.html', {'response': response})
 
 
@@ -40,6 +40,7 @@ def login_user(request):
         else:
             messages.error(request, 'Username or password incorrect.')
     return render(request, 'login.html', {'response': response})
+
 
 @find_worth
 def peasant(request):
@@ -65,6 +66,7 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
 
 def grab(request, item_id):
     uuid = item_id
